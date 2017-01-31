ec0321fff5606bd61d086eaad393212db1a8f9aa
daveho8888 <daveho8888@gmail.com>
2017-01-30 20:35:11 -0800

    remove more

--- Piptrix/App/ui.R
+++ Piptrix/App/ui.R
@@ -1,16 +1,6 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(shinyIncubator)
 
-# Define UI for application that draws a histogram
 shinyUI(
   navbarPage("Piptrix",
              theme="bootstrap.css",
